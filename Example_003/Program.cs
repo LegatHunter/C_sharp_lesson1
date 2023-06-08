341373142f7086e2c4268471f57aec90e89a2354
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-06-08 18:17:34 +0300

    Рандом в 3 примере

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numderA = 3;
-int numberB = 5;
+﻿int numderA = new Random().Next(1, 10); // 1 2 3 ... 9
+System.Console.WriteLine(numderA);
+int numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
 int result = numderA + numberB;
 Console.WriteLine(result);
