a576f894d46e2afbde6bb474fb342df4f4c4d62c
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-06-09 09:28:13 +0300

    Добавили if else

---
+++ Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() =="никита")
+{
+    Console.WriteLine("Привет красавчик");
+}
+else
+{
+    System.Console.Write("Да мне пофиг, ");
+    System.Console.WriteLine(username);
+}
