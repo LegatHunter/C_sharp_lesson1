b0fe0a8df824c25aba6a9b31e9ce7c30f79862fd
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-06-13 20:07:40 +0300

    Example_011

---
+++ Example_011/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 13, 45, 68, 62, 45, 77 };
+int n = array.Length;
+int find = 13;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
